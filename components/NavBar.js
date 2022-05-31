228fc4e9f9d8ae868f4dbb3d3ea414848276e248
Javier Palina <javiariel2003@live.com.ar>
2022-05-30 21:29:58 -0300

    segundo desafío

---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+    <header>
+        <nav>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        </nav>
+    </header>)
+}
+
+export default Header
---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return (
+        <header>
+            <h1 className="brand">ViperG</h1>
+            <ul>
+                <li>
+                    <a href="#">Link 1</a>
+                </li>
+                <li>
+                    <a href="#">Link 2</a>
+                </li>
+                <li>
+                    <a href="#">Link 3</a>
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default NavBar
